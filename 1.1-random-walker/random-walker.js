629a0b0c3a92c183a9ec195b4308423663a1fb12
Edgar <drarbego@gmail.com>
2022-10-27 19:40:26 -0500

    create basic random walker

--- 1.1-random-walker/random-walker.js
+++ 1.1-random-walker/random-walker.js
@@ -1,22 +1,50 @@
 class Walker {
-    constructor() { 
-        this.x = 0;
-        this.y = 0;
+    constructor(x, y) { 
+        this.x = x;
+        this.y = y;
     }
 
     display() {
         stroke(0);
         point(this.x, this.y)
     }
+
+    randomStep() {
+        let choice = parseInt(random(4));
+        switch(choice){
+            case 0:
+                this.x += 1;
+                break;
+            case 1:
+                this.x -= 1;
+                break;
+            case 2:
+                this.y += 1;
+                break;
+            case 3:
+                this.y -= 1;
+                break;
+        }
+    }
+
+    improvedRandomStep() {
+        let stepX = random(-1, 1);
+        let stepY = random(-1, 1);
+
+        this.x += stepX;
+        this.y += stepY;
+    }
 }
 
-let walker = new Walker();
-
+let walker = null;
 
 function setup() {
     createCanvas(400, 400);
+    background(220);
+    walker = new Walker(width/2, height/2);
 }
   
 function draw() {
-    background(220);
+    walker.display();
+    walker.improvedRandomStep();
 }
